e7552a9d9b1c7b5f4d9794a494490e7f83e5bb53
Victor Lourme <victor.lourme@icloud.com>
2020-06-30 15:37:50 +0200

    Package: add Moya as dependency

--- Package.swift
+++ Package.swift
@@ -21,7 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SwiftyShoppy",
-            dependencies: []),
+            dependencies: ["Moya"]),
         .testTarget(
             name: "SwiftyShoppyTests",
             dependencies: ["SwiftyShoppy"]),
