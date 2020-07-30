e74315bdb2af4ef3d5ebcdfcdeed8e9c811fd450
Victor Lourme <victor.lourme@icloud.com>
2020-07-30 23:53:04 +0200

    🔧 Bump to 1.2.0

--- SwiftyShoppy/API.swift
+++ SwiftyShoppy/API.swift
@@ -126,7 +126,7 @@ extension Shoppy : TargetType, AccessTokenAuthorizable {
     
     public var headers: [String : String]? {
         return [
-            "User-Agent": "SwiftyShoppy/1.1.9"
+            "User-Agent": "SwiftyShoppy/1.2.0"
         ]
     }
 }
